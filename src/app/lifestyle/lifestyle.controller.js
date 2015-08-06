184e51c6a5a5af601ecc5bce25746f5143c7592b
Evan <evan@Janes-MacBook-Pro.local>
2015-08-05 21:25:44 -0400

    not using trolly parallax libs

--- src/app/lifestyle/lifestyle.controller.js
+++ src/app/lifestyle/lifestyle.controller.js
@@ -1,6 +1,5 @@
 'use strict';
 
 angular.module("capecrossing")
-  .controller("LifestyleCtrl", ['$scope', 'parallaxHelper', function ($scope, parallaxHelper) {
-  	$scope.background = parallaxHelper.createAnimator(-0.5, 180, -550, 500);
-  }]);
+  .controller("LifestyleCtrl", function ($scope) {
+  });
